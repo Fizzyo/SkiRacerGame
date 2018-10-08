5bb4358dbb8b642ac3d4925fb623eee77197a008
Lee Stott <leestott@microsoft.com>
2018-10-08 06:37:26 -0700

    Updates
    Based on Tiernan updates

---
+++ game_demo/Assets/Scripts/BreathsCount.cs
@@ -0,0 +1,10 @@
+public static class BreathsCount
+{
+    private static int breathsPer;
+
+    public static int BreathsPer
+    {
+        get { return breathsPer; }
+        set { breathsPer = value; }
+    }
+}
